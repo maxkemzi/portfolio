b8318010a11524d7cc5470934042d1f9b7bbd131
Max Kyrychenko <iam.maxkyrychenko@gmail.com>
2024-08-24 07:54:29 +0300

    add test coverage script

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,13 @@ import path from 'node:path';
 import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
-	test: {globals: true},
+	test: {
+		globals: true,
+		coverage: {
+			provider: 'v8',
+			reporter: ['html'],
+		},
+	},
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
