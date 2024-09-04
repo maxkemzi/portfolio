ff426ae661ffe50adbee404e6ce80540aee0a512
Max Kyrychenko <iam.maxkyrychenko@gmail.com>
2024-09-04 15:09:34 +0300

    set up react testing library

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,17 @@
 import path from 'node:path';
 import {defineConfig} from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+	plugins: [react()],
 	test: {
 		globals: true,
 		coverage: {
 			provider: 'v8',
 			reporter: ['html'],
 		},
+		environment: 'jsdom',
+		setupFiles: './test/setup.ts',
 	},
 	resolve: {
 		alias: {
