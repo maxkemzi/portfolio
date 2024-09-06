a484f84446b966f679892cd2af7aca99eee3ebec
Max Kyrychenko <iam.maxkyrychenko@gmail.com>
2024-09-06 16:50:06 +0300

    add tests for ContactForm component

---
+++ test/src/components/ContactForm/ContactForm.test.tsx
@@ -0,0 +1,116 @@
+import {render, screen} from '@testing-library/react';
+import {ContactForm} from '@/components';
+import {useAction} from 'next-safe-action/hooks';
+import userEvent from '@testing-library/user-event';
+import {Mock} from 'vitest';
+
+vi.mock('next-safe-action/hooks', () => ({
+	useAction: vi.fn(),
+}));
+
+vi.mock('@/components/ContactForm/actions', () => ({
+	sendContactMail: vi.fn(() => Promise.resolve()),
+}));
+
+const USE_ACTION_RETURN_VALUE = {
+	execute: vi.fn(),
+	isExecuting: false,
+	hasSucceeded: false,
+	hasErrored: false,
+};
+const mockUseAction = (useAction as Mock).mockReturnValue(
+	USE_ACTION_RETURN_VALUE,
+);
+
+afterEach(() => {
+	vi.clearAllMocks();
+});
+
+test('renders form elements correctly', () => {
+	render(<ContactForm />);
+
+	expect(screen.getByLabelText('Name')).toBeInTheDocument();
+	expect(screen.getByLabelText('Email')).toBeInTheDocument();
+	expect(screen.getByLabelText('Message')).toBeInTheDocument();
+	expect(screen.getByRole('button', {name: /submit/i})).toBeInTheDocument();
+});
+
+test('displays validation errors when submitting invalid form', async () => {
+	render(<ContactForm />);
+
+	await userEvent.click(screen.getByRole('button', {name: /submit/i}));
+
+	expect(screen.getByText('Name is required')).toBeInTheDocument();
+	expect(screen.getByText('Email is required')).toBeInTheDocument();
+	expect(screen.getByText('Message is required')).toBeInTheDocument();
+});
+
+test('submits the form successfully with valid data', async () => {
+	const mockExecute = vi.fn();
+	mockUseAction.mockReturnValue({
+		...USE_ACTION_RETURN_VALUE,
+		execute: mockExecute,
+	});
+	render(<ContactForm />);
+
+	await userEvent.type(screen.getByLabelText('Name'), 'John Doe');
+	await userEvent.type(screen.getByLabelText('Email'), 'john@example.com');
+	await userEvent.type(
+		screen.getByLabelText('Message'),
+		'We would love to work with you!',
+	);
+	await userEvent.click(screen.getByRole('button', {name: /submit/i}));
+
+	expect(mockExecute).toHaveBeenCalledWith({
+		name: 'John Doe',
+		email: 'john@example.com',
+		message: 'We would love to work with you!',
+	});
+});
+
+test('disables the submit button when form is submitting', async () => {
+	mockUseAction.mockReturnValue({
+		...USE_ACTION_RETURN_VALUE,
+		isExecuting: true,
+	});
+	render(<ContactForm />);
+
+	const submitButton = screen.getByRole('button', {name: /submit/i});
+	expect(submitButton).toBeDisabled();
+});
+
+test('displays success message after form submission', async () => {
+	mockUseAction.mockReturnValue({
+		...USE_ACTION_RETURN_VALUE,
+		hasSucceeded: true,
+	});
+	render(<ContactForm />);
+
+	await userEvent.type(screen.getByLabelText('Name'), 'John Doe');
+	await userEvent.type(screen.getByLabelText('Email'), 'john@example.com');
+	await userEvent.type(
+		screen.getByLabelText('Message'),
+		'We would love to work with you!',
+	);
+	await userEvent.click(screen.getByRole('button', {name: /submit/i}));
+
+	expect(screen.getByText('Form has been submitted.')).toBeInTheDocument();
+});
+
+test('displays error message after form submission failure', async () => {
+	mockUseAction.mockReturnValue({
+		...USE_ACTION_RETURN_VALUE,
+		hasErrored: true,
+	});
+	render(<ContactForm />);
+
+	await userEvent.type(screen.getByLabelText('Name'), 'John Doe');
+	await userEvent.type(screen.getByLabelText('Email'), 'john@example.com');
+	await userEvent.type(
+		screen.getByLabelText('Message'),
+		'We would love to work with you!',
+	);
+	await userEvent.click(screen.getByRole('button', {name: /submit/i}));
+
+	expect(screen.getByText('Something went wrong.')).toBeInTheDocument();
+});
