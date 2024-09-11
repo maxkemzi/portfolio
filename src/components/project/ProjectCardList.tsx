449c2ad4079a9d09cba2beb2c56581b69a451fe1
Max Kyrychenko <iam.maxkyrychenko@gmail.com>
2024-09-11 15:07:43 +0300

    fix mistake in the calculation of project card height

--- src/components/project/ProjectCardList.tsx
+++ src/components/project/ProjectCardList.tsx
@@ -11,18 +11,18 @@ const COLS = 2;
 const calcItemHeight = (index: number, length: number) => {
 	const SMALL_HEIGHT = 350;
 	const LARGE_HEIGHT = 450;
-	const rowIndex = index % COLS;
 
-	const distributionIsEven = length % COLS === 0;
-	const itemInFirstHalf = index < Math.ceil(length / 2);
+	const rows = Math.ceil(length / COLS);
+
+	const rowIndex = index % rows;
+	const columnIndex = Math.floor(index / rows);
 
 	const evenPattern = [SMALL_HEIGHT, LARGE_HEIGHT];
 	const oddPattern = [LARGE_HEIGHT, SMALL_HEIGHT];
 
-	const heightPattern =
-		distributionIsEven || itemInFirstHalf ? evenPattern : oddPattern;
+	const heightPattern = columnIndex % 2 === 0 ? evenPattern : oddPattern;
 
-	return heightPattern[rowIndex];
+	return heightPattern[rowIndex % 2];
 };
 
 const ProjectCardList = forwardRef(
